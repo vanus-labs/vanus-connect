71b540c19a8237b4e7ea12b9d32d32d486c8bb7d
Jie Ding <jie.ding@linkall.com>
2022-06-14 19:26:01 +0800

    feat: delete feishu bot
    Signed-off-by: Jie Ding <jie.ding@linkall.com>

--- connectors/sink-feishu-bot/src/main/java/com/linkall/sink/feishubot/Entrance.java
+++
@@ -1,9 +0,0 @@
-package com.linkall.sink.feishubot;
-
-import com.linkall.core.VanceApplication;
-
-public class Entrance {
-    public static void main(String[] args) {
-        VanceApplication.run(FeishuBotSink.class);
-    }
-}
