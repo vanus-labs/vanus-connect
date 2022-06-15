a51d660b01b0482814ee5eeba4fdf9416584432a
Jie Ding <jie.ding@linkall.com>
2022-06-15 17:24:02 +0800

    feat: add feishubot
    Signed-off-by: Jie Ding <jie.ding@linkall.com>

--- connectors/sink-feishu-bot/src/main/java/com/linkall/sink/feishubot/Entrance.java
+++ connectors/sink-feishu-bot/src/main/java/com/linkall/sink/feishubot/Entrance.java
@@ -1,6 +1,6 @@
 package com.linkall.sink.feishubot;
 
-import com.linkall.core.VanceApplication;
+import com.linkall.vance.core.VanceApplication;
 
 public class Entrance {
     public static void main(String[] args) {
