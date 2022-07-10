8819daf225da3bcacc85517a9d13363677c982ec
Jie Ding <jie.ding@linkall.com>
2022-07-10 19:11:06 +0800

    feat: add a new connector-Display
    Signed-off-by: Jie Ding <jie.ding@linkall.com>

---
+++ connectors/sink-display/src/main/java/com/linkall/sink/display/Entrance.java
@@ -0,0 +1,9 @@
+package com.linkall.sink.display;
+
+import com.linkall.vance.core.VanceApplication;
+
+public class Entrance {
+    public static void main(String[] args) {
+        VanceApplication.run(DisplaySink.class);
+    }
+}
