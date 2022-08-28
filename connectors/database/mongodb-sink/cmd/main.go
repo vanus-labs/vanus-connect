9345891b9ea4bf0f4ade0117fa831eaf6f608440
wenfeng <sxian.wang@gmail.com>
2022-08-28 16:25:17 +0800

    update mongodb sink

--- connectors/database/mongodb-sink/cmd/main.go
+++ connectors/database/mongodb-sink/cmd/main.go
@@ -1,26 +1,24 @@
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"context"
+	"github.com/linkall-labs/connector/cdk-go"
 	"github.com/linkall-labs/connector/mongodb-sink/internal"
-	"os"
 )
 
 func main() {
-	ga := internal.NewMongoSink(internal.Config{Port: 8081})
-	err := ga.StartReceive(context.Background())
-	if err != nil {
-		//log.Error(context.Background(), "start controller proxy failed", map[string]interface{}{
-		//	log.KeyError: err,
-		//})
-		os.Exit(-1)
-	}
-
-	err = ga.StartReceive(context.Background())
-	if err != nil {
-		//log.Error(context.Background(), "start CloudEvents gateway failed", map[string]interface{}{
-		//	log.KeyError: err,
-		//})
-		os.Exit(-1)
-	}
+	cdkgo.RunSink(internal.NewMongoSink())
 }
--- connectors/database/mongodb-sink/internal/server.go
+++ connectors/database/mongodb-sink/internal/server.go
@@ -17,51 +17,167 @@ package internal
 import (
 	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"strings"
+	"time"
+
 	v2 "github.com/cloudevents/sdk-go/v2"
 	"github.com/cloudevents/sdk-go/v2/protocol"
-	"github.com/golang/protobuf/jsonpb"
-	proto "github.com/linkall-labs/connector/mongodb-sink/database"
-	"net"
-
-	"github.com/cloudevents/sdk-go/v2/client"
 	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
+	"github.com/golang/protobuf/jsonpb"
+	"github.com/linkall-labs/connector/cdk-go/runtime"
+	proto "github.com/linkall-labs/connector/mongodb-sink/proto/database"
+	"github.com/linkall-labs/connector/proto/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	mongoSinkDatabase   = "vancemongosinkdatabase"
+	mongoSinkCollection = "vancemongosinkcollection"
 )
 
 type Config struct {
-	Port int
+	Port    int      `json:"port"`
+	DBHosts []string `json:"db_hosts"`
+	Secret  Secret   `json:"-"`
 }
 
 type Secret struct {
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	AuthSource string `json:"auth_source"`
+}
+
+func (sc Secret) isSet() bool {
+	return sc.Username != "" || sc.Password != "" || sc.AuthSource != ""
 }
 
 type sink struct {
-	cfg *Config
+	cfg      *Config
+	dbClient *mongo.Client
 }
 
-func NewMongoSink(config Config) *sink {
-	return &sink{
-		cfg: &config,
+func NewMongoSink() runtime.Sink {
+	return &sink{}
+}
+
+func (s *sink) Init(cfgPath, secretPath string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	opts := options.Client()
+	opts.ApplyURI(fmt.Sprintf("mongodb://%s", strings.Join(s.cfg.DBHosts, ",")))
+	if s.cfg.Secret.isSet() {
+		opts.Auth = &options.Credential{
+			AuthSource:  s.cfg.Secret.AuthSource,
+			Username:    s.cfg.Secret.Username,
+			Password:    s.cfg.Secret.Password,
+			PasswordSet: false,
+		}
 	}
-}
-
-func (s *sink) StartReceive(ctx context.Context) error {
-	ls, err := net.Listen("tcp", fmt.Sprintf(":%d", s.cfg.Port))
+	mongoClient, err := mongo.Connect(ctx, opts)
 	if err != nil {
-		return err
+		panic("connect to mongodb error: " + err.Error())
 	}
-
-	c, err := client.NewHTTP(cehttp.WithListener(ls), cehttp.WithRequestDataAtContextMiddleware())
-	if err != nil {
-		return err
-	}
-	return c.StartReceiver(ctx, s.receive)
+	s.dbClient = mongoClient
+	return nil
 }
 
-func (s *sink) receive(ctx context.Context, event v2.Event) protocol.Result {
+func (s *sink) Destroy() error {
+	return nil
+}
+
+func (s *sink) Name() string {
+	return "mongodb-sink"
+}
+
+func (s *sink) Port() int {
+	return s.cfg.Port
+}
+
+func (s *sink) Handle(ctx context.Context, event *v2.Event) protocol.Result {
+	extensions := event.Extensions()
+	dbName, exist := extensions[mongoSinkDatabase]
+	if !exist {
+		return cehttp.NewResult(http.StatusBadRequest, "vancemongosinkdatabase is empty")
+	}
+
+	collName, exist := extensions[mongoSinkCollection]
+	if !exist {
+		return cehttp.NewResult(http.StatusBadRequest, "vancemongosinkcollection is empty")
+	}
+	id, err := primitive.ObjectIDFromHex(event.ID())
+	if err != nil {
+		return cehttp.NewResult(http.StatusBadRequest,
+			fmt.Sprintf("invalid id %s, hex mongo id required", err))
+	}
+
 	e := &proto.Event{}
-	_ = jsonpb.Unmarshal(bytes.NewReader(event.Data()), e)
-	d, _ := event.MarshalJSON()
-	println(string(d))
-	return cehttp.NewResult(200, "")
+	if err := jsonpb.Unmarshal(bytes.NewReader(event.Data()), e); err != nil {
+		return cehttp.NewResult(http.StatusBadRequest, err.Error())
+	}
+
+	if err := s.validate(e); err != nil {
+		return cehttp.NewResult(http.StatusBadRequest, err.Error())
+	}
+
+	switch e.Op {
+	case database.Operation_INSERT:
+		err = s.insert(ctx, fmt.Sprintf("%s", dbName), fmt.Sprintf("%s", collName), e)
+	case database.Operation_UPDATE:
+		err = s.update(ctx, id, fmt.Sprintf("%s", dbName), fmt.Sprintf("%s", collName), e)
+	case database.Operation_DELETE:
+		err = s.delete(ctx, id, fmt.Sprintf("%s", dbName), fmt.Sprintf("%s", collName))
+	default:
+		return cehttp.NewResult(http.StatusBadRequest, fmt.Sprintf("unsupported event operation: %s", e.Op))
+	}
+
+	if err != nil {
+		return cehttp.NewResult(http.StatusInternalServerError, err.Error())
+	}
+	return cehttp.NewResult(http.StatusOK, "")
+}
+
+func (s *sink) insert(ctx context.Context, dbName, collName string, e *proto.Event) error {
+	_, err := s.dbClient.Database(dbName).Collection(collName).InsertOne(ctx, e.Insert.Document)
+	return err
+}
+
+func (s *sink) update(ctx context.Context, id primitive.ObjectID, dbName, collName string, e *proto.Event) error {
+	data, err := e.Update.UpdateDescription.UpdatedFields.MarshalJSON()
+	if err != nil {
+		return fmt.Errorf("try to marhsall UpdatedFields error: %s", err)
+	}
+	updates := make(map[string]interface{})
+	if err = json.Unmarshal(data, &updates); err != nil {
+		return fmt.Errorf("try to unmarhsall UpdatedFields data to map error: %s", err)
+	}
+
+	_, err = s.dbClient.Database(dbName).Collection(collName).UpdateOne(ctx,
+		bson.M{"_id": id},
+		updates)
+	return err
+}
+
+func (s *sink) delete(ctx context.Context, id primitive.ObjectID, dbName, collName string) error {
+	_, err := s.dbClient.Database(dbName).Collection(collName).DeleteOne(ctx, bson.M{
+		"_id": id,
+	})
+	return err
+}
+
+func (s *sink) validate(event *proto.Event) error {
+	if event.Op == database.Operation_INSERT && event.Insert == nil {
+		return fmt.Errorf("invalid insert event")
+	}
+
+	if event.Op == database.Operation_UPDATE && event.Update == nil {
+		return fmt.Errorf("invalid update event")
+	}
+
+	return nil
 }
