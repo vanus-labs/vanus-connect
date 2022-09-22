e8e99c1d09f8c1ad1e3ee712c537c65087663f38
sling <18102030131@163.com>
2022-09-22 16:41:13 +0800

    rm example
    Signed-off-by: sling <18102030131@163.com>

--- generator/source-sns/src/main/java/com/vance/source/sns/Entrance.java
+++
@@ -1,9 +0,0 @@
-package com.vance.source.sns;
-
-import com.linkall.vance.core.VanceApplication;
-
-public class Entrance {
-    public static void main(String[] args) {
-        VanceApplication.run(SnsSource.class);
-    }
-}
