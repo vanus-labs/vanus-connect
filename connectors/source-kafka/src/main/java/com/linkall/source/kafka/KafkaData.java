dfb3a5cc452ec0766e082061d82e8ff897f3078c
MikeG <62085623+Michaelg22@users.noreply.github.com>
2022-10-28 16:05:50 +0800

    fix: change key from a string to bye[]

--- connectors/source-kafka/src/main/java/com/linkall/source/kafka/KafkaData.java
+++ connectors/source-kafka/src/main/java/com/linkall/source/kafka/KafkaData.java
@@ -2,7 +2,7 @@ package com.linkall.source.kafka;
 
 import java.time.OffsetDateTime;
 
-public record KafkaData(String topic, String key, byte[] value, String KAFKA_SERVER_URL, OffsetDateTime timeStamp) {
+public record KafkaData(String topic, byte[] key, byte[] value, String KAFKA_SERVER_URL, OffsetDateTime timeStamp) {
 
 
 }
