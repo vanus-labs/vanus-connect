db1ada5da02b4763e08b39532ff3644785448b5e
wenfeng <sxian.wang@gmail.com>
2022-11-02 19:41:07 +0800

    update

--- connectors/source-tencentcloud-cos/internal/function/main.go
+++ connectors/source-tencentcloud-cos/internal/function/main.go
@@ -100,6 +100,6 @@ func main() {
 		funcName: fName,
 		debug:    dg,
 	}
-	// Make the handler available for Remote Procedure Call by Cloud Code
+	// Make the handler available for Remote Procedure Call by Cloud Function
 	cloudfunction.Start(w.receive)
 }
