cffcb6ed9c4be3826648e9403c584e18e05b9041
wenfeng <sxian.wang@gmail.com>
2022-12-21 14:39:59 +0800

    feat: add slack sink
    Signed-off-by: wenfeng <sxian.wang@gmail.com>

---
+++ connectors/sink-slack/go.mod
@@ -0,0 +1,46 @@
+module github.com/linkall-labs/connector/sink/slack
+
+go 1.18
+
+require (
+	github.com/cloudevents/sdk-go/v2 v2.12.0
+	github.com/linkall-labs/cdk-go v0.3.3
+	github.com/nikoksr/notify v0.36.0
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/slack-go/slack v0.11.4 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.6 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.6 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.6 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
+	golang.org/x/crypto v0.2.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
---
+++ connectors/sink-slack/internal/server.go
@@ -0,0 +1,183 @@
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"strings"
+	"time"
+
+	v2 "github.com/cloudevents/sdk-go/v2"
+	cdkgo "github.com/linkall-labs/cdk-go"
+	"github.com/linkall-labs/cdk-go/log"
+	"github.com/nikoksr/notify/service/slack"
+	"github.com/pkg/errors"
+)
+
+const (
+	name            = "Slack Sink"
+	xvSlackApp      = "xvslackapp"
+	xvSlackChannels = "xvslackchannels"
+)
+
+var (
+	errInvalidMessage = cdkgo.NewResult(http.StatusBadRequest,
+		"slack: invalid message body")
+	errInvalidChannels = cdkgo.NewResult(http.StatusBadRequest,
+		"slack: invalid or empty channels")
+	errInvalidAppName = cdkgo.NewResult(http.StatusBadRequest,
+		"slack: invalid or empty AppName")
+	errFailedToSend = cdkgo.NewResult(http.StatusInternalServerError,
+		"slack: failed to sent message, please view logs")
+)
+
+var _ cdkgo.SinkConfigAccessor = &slackConfig{}
+
+type SlackConfig struct {
+	AppName        string `yaml:"app_name" json:"app_name"`
+	Token          string `yaml:"token" json:"token"`
+	DefaultChannel string `yaml:"default_channel" json:"default_channel"`
+}
+
+type slackConfig struct {
+	cdkgo.SinkConfig `json:",inline" yaml:",inline"`
+	DefaultAppID     string        ` json:"default" yaml:"default"`
+	Slacks           []SlackConfig `json:"slack" yaml:"slack" validate:"dive,gt=0"`
+}
+
+func NewConfig() cdkgo.SinkConfigAccessor {
+	return &slackConfig{}
+}
+
+func NewSlackSink() cdkgo.Sink {
+	return &slackSink{
+		apps: map[string]SlackConfig{},
+	}
+}
+
+var _ cdkgo.Sink = &slackSink{}
+
+type slackSink struct {
+	cfg   *slackConfig
+	count int64
+	apps  map[string]SlackConfig
+}
+
+func (e *slackSink) Arrived(ctx context.Context, events ...*v2.Event) cdkgo.Result {
+	for idx := range events {
+		event := events[idx]
+
+		var appID string
+		v, exist := event.Extensions()[xvSlackApp]
+		if exist {
+			str, ok := v.(string)
+			if !ok {
+				return errInvalidAppName
+			}
+			appID = str
+		} else if e.cfg.DefaultAppID != "" {
+			appID = e.cfg.DefaultAppID
+		}
+
+		c, ok := e.apps[appID]
+		if !ok {
+			return errInvalidAppName
+		}
+
+		var ids string
+		v, exist = event.Extensions()[xvSlackChannels]
+		if exist {
+			str, ok := v.(string)
+			if !ok {
+				return errInvalidChannels
+			}
+			ids = str
+		} else if c.DefaultChannel != "" {
+			ids = c.DefaultChannel
+		}
+
+		channels := strings.Split(ids, ",")
+		if len(channels) == 0 {
+			return errInvalidChannels
+		}
+
+		m := &Message{}
+		if err := json.Unmarshal(event.Data(), m); err != nil {
+			return errInvalidMessage
+		}
+
+		start := time.Now()
+		if err := e.send(ctx, c.Token, m, channels...); err != nil {
+			log.Error("failed to send slack", map[string]interface{}{
+				log.KeyError: err,
+				"channels":   channels,
+				"event_id":   event.ID(),
+			})
+			return errFailedToSend
+		} else if time.Now().Sub(start) > time.Second {
+			log.Debug("success to send slack, but takes too long", map[string]interface{}{
+				"channels":  channels,
+				"event_id":  event.ID(),
+				"used_time": time.Now().Sub(start),
+			})
+		} else {
+			log.Debug("success to send slack", map[string]interface{}{
+				"channels": channels,
+				"event_id": event.ID(),
+			})
+		}
+	}
+	return cdkgo.SuccessResult
+}
+
+func (e *slackSink) Initialize(_ context.Context, cfg cdkgo.ConfigAccessor) error {
+	_cfg, ok := cfg.(*slackConfig)
+	if !ok {
+		return errors.New("slack: invalid configuration type")
+	}
+
+	e.cfg = _cfg
+	for _, m := range _cfg.Slacks {
+		e.apps[m.AppName] = m
+	}
+	if e.cfg.DefaultAppID != "" {
+		if _, exist := e.apps[e.cfg.DefaultAppID]; !exist {
+			return errors.New("slack: the default slack config isn't exist")
+		}
+	}
+	return nil
+}
+
+func (e *slackSink) Name() string {
+	return name
+}
+
+func (e *slackSink) Destroy() error {
+	// nothing to do
+	return nil
+}
+
+func (e *slackSink) send(ctx context.Context, token string, msg *Message, channels ...string) error {
+	slackService := slack.New(token)
+	slackService.AddReceivers(channels...)
+	return slackService.Send(ctx, msg.Subject, msg.Message)
+}
+
+type Message struct {
+	Subject string
+	Message string
+}
