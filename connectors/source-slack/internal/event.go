0f8b75ccabe5f06f5c6795c965ebcc0e2fe2e708
delu <delu.xu@linkall.com>
2023-04-24 14:34:14 +0800

    feat: add source slack (#344)
    Signed-off-by: xdlbdy <xdlbdy@gmail.com>

---
+++ connectors/source-slack/internal/event.go
@@ -0,0 +1,141 @@
+// Copyright 2023 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+
+	"github.com/vanus-labs/cdk-go/log"
+
+	cdkgo "github.com/vanus-labs/cdk-go"
+
+	ce "github.com/cloudevents/sdk-go/v2"
+	"github.com/google/uuid"
+)
+
+var (
+	errMissingHubSignatureHeader = errors.New("missing signature Header")
+	errInvalidSignatureHeader    = errors.New("invalid signature Header")
+	errInvalidContentTypeHeader  = errors.New("only support application/json Content-Type Header")
+	errReadPayload               = errors.New("error read payload")
+	errVerificationFailed        = errors.New("signature verification failed")
+	errVerificationTokenFailed   = errors.New("token verification failed")
+)
+
+func (s *slackSource) verifyRequestSignature(req *http.Request, body []byte) error {
+	sv, err := slack.NewSecretsVerifier(req.Header, s.config.SigningSecret)
+	if err != nil {
+		if err == slack.ErrMissingHeaders {
+			return errMissingHubSignatureHeader
+		}
+		log.Info("new secret verifier failed", map[string]interface{}{
+			log.KeyError: err,
+		})
+		return errInvalidSignatureHeader
+	}
+	_, _ = sv.Write(body)
+	if err := sv.Ensure(); err != nil {
+		return errVerificationFailed
+	}
+	return nil
+}
+
+func (s *slackSource) event(w http.ResponseWriter, req *http.Request) error {
+	contentType := req.Header.Get("Content-Type")
+	if contentType != "application/json" {
+		return errInvalidContentTypeHeader
+	}
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil || len(body) == 0 {
+		return errReadPayload
+	}
+	err = s.verifyRequestSignature(req, body)
+	if err != nil {
+		return err
+	}
+	var event map[string]interface{}
+	err = json.Unmarshal(body, &event)
+	if err != nil {
+		return err
+	}
+	token, err := getStringValue(event, "token")
+	if err != nil {
+		return err
+	}
+	if !s.verifyToken.Verify(token) {
+		return errVerificationTokenFailed
+	}
+	eventType, err := getStringValue(event, "type")
+	if err != nil {
+		return err
+	}
+	if eventType == slackevents.CallbackEvent {
+		// https://api.slack.com/apis/connections/events-api#events-JSON
+		s.makeEvent(event)
+		w.Write([]byte("accepted"))
+		return nil
+	}
+	if eventType == slackevents.URLVerification {
+		// https://api.slack.com/apis/connections/events-api#verification
+		challenge, err := getStringValue(event, "challenge")
+		if err != nil {
+			return err
+		}
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write([]byte(challenge))
+		return nil
+	}
+	return nil
+}
+
+func getEventType(body map[string]interface{}) string {
+	event, ok := body["event"].(map[string]interface{})
+	if !ok {
+		return ""
+	}
+	eventType, _ := getStringValue(event, "type")
+	return eventType
+}
+
+func (s *slackSource) makeEvent(body map[string]interface{}) error {
+	eventType := getEventType(body)
+	event := ce.NewEvent()
+	event.SetID(uuid.NewString())
+	event.SetSource(fmt.Sprintf("https://github.com/vanus-labs/vanus-connect/connectors/source-slack"))
+	event.SetType("event_callback")
+	event.SetExtension("eventtype", eventType)
+	delete(body, "token")
+	event.SetData(ce.ApplicationJSON, body)
+
+	s.ch <- &cdkgo.Tuple{
+		Event: &event,
+		Success: func() {
+			log.Info("send event success", nil)
+		},
+		Failed: func(err error) {
+			log.Info("send event failed", map[string]interface{}{
+				log.KeyError: err,
+			})
+		},
+	}
+	return nil
+}
---
+++ connectors/source-slack/internal/server.go
@@ -0,0 +1,109 @@
+// Copyright 2022 Linkall Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package internal
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/slack-go/slack/slackevents"
+
+	cdkgo "github.com/vanus-labs/cdk-go"
+	"github.com/vanus-labs/cdk-go/log"
+)
+
+const (
+	name        = "Slack Source"
+	defaultPort = 8080
+)
+
+var _ cdkgo.Source = &slackSource{}
+
+func NewSource() cdkgo.Source {
+	return &slackSource{
+		ch: make(chan *cdkgo.Tuple, 1024),
+	}
+}
+
+type slackSource struct {
+	config      *slackConfig
+	ch          chan *cdkgo.Tuple
+	server      *http.Server
+	verifyToken slackevents.TokenComparator
+}
+
+func (s *slackSource) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("not support request method"))
+		return
+	}
+	err := s.event(w, req)
+	if err == nil {
+		return
+	}
+	log.Info("event error", map[string]interface{}{
+		log.KeyError: err,
+	})
+	var code int
+	switch err {
+	case errVerificationFailed, errVerificationTokenFailed:
+		code = http.StatusForbidden
+	default:
+		code = http.StatusBadRequest
+	}
+	w.WriteHeader(code)
+	w.Write([]byte(err.Error()))
+}
+
+func (s *slackSource) Chan() <-chan *cdkgo.Tuple {
+	return s.ch
+}
+
+func (s *slackSource) Initialize(_ context.Context, cfg cdkgo.ConfigAccessor) error {
+	s.config = cfg.(*slackConfig)
+	if s.config.Port == 0 {
+		s.config.Port = defaultPort
+	}
+	s.verifyToken = slackevents.TokenComparator{
+		VerificationToken: s.config.VerifyToken,
+	}
+	s.server = &http.Server{
+		Addr:    fmt.Sprintf(":%d", s.config.Port),
+		Handler: s,
+	}
+	go func() {
+		log.Info("http server is ready to start", map[string]interface{}{
+			"port": s.config.Port,
+		})
+		if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			panic(fmt.Sprintf("cloud not listen on %d, error:%s", s.config.Port, err.Error()))
+		}
+		log.Info("http server stopped", nil)
+	}()
+	return nil
+}
+
+func (s *slackSource) Name() string {
+	return name
+}
+
+func (s *slackSource) Destroy() error {
+	if s.server != nil {
+		return s.server.Shutdown(context.Background())
+	}
+	return nil
+}
