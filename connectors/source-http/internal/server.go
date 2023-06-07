85914ca7c6ae705703c505a493c5283446ab052a
xdlbdy <xdlbdy@gmail.com>
2023-06-07 19:54:13 +0800

    feat: source http share
    Signed-off-by: xdlbdy <xdlbdy@gmail.com>

--- connectors/source-http/internal/server.go
+++ connectors/source-http/internal/server.go
@@ -15,21 +15,17 @@
 package internal
 
 import (
-	"bufio"
-	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
 	"sync"
 
 	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 
 	v2 "github.com/cloudevents/sdk-go/v2"
-	"github.com/valyala/fasthttp"
 
 	cdkgo "github.com/vanus-labs/cdk-go"
 	"github.com/vanus-labs/cdk-go/log"
@@ -104,11 +100,11 @@ func (c *httpSource) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	he := &HTTPEvent{
 		Path:      req.RequestURI,
 		Method:    req.Method,
-		QueryArgs: getQueryArgsNew(req),
-		Headers:   getHeadersNew(req),
+		QueryArgs: getQueryArgs(req),
+		Headers:   getHeaders(req),
 	}
 	e := v2.NewEvent()
-	mappingAttributesNew(req, he, &e)
+	mappingAttributes(req, he, &e)
 
 	// try to convert request.Body to json
 	m := map[string]interface{}{}
@@ -139,20 +135,22 @@ func (c *httpSource) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	c.ch <- &cdkgo.Tuple{
 		Event: &e,
 		Success: func() {
+			defer wg.Done()
+			c.logger.Info().Str("event_id", e.ID()).Msg("send event to target success")
 			w.WriteHeader(http.StatusOK)
-			wg.Done()
 		},
 		Failed: func(err2 error) {
+			defer wg.Done()
+			c.logger.Warn().Interface("event_id", e.ID()).Err(err2).Msg("failed to send event to target")
 			w.WriteHeader(http.StatusInternalServerError)
 			w.Write([]byte(
 				fmt.Sprintf("failed to send event to remote server: %s", err2.Error())))
-			wg.Done()
 		},
 	}
 	wg.Wait()
 }
 
-func getQueryArgsNew(req *http.Request) map[string]string {
+func getQueryArgs(req *http.Request) map[string]string {
 	m := map[string]string{}
 	values := req.URL.Query()
 	for key, value := range values {
@@ -161,7 +159,7 @@ func getQueryArgsNew(req *http.Request) map[string]string {
 	return m
 }
 
-func getHeadersNew(req *http.Request) map[string]string {
+func getHeaders(req *http.Request) map[string]string {
 	m := map[string]string{}
 	headers := req.Header
 	for key, header := range headers {
@@ -173,7 +171,7 @@ func getHeadersNew(req *http.Request) map[string]string {
 	return m
 }
 
-func mappingAttributesNew(req *http.Request, he *HTTPEvent, e *v2.Event) {
+func mappingAttributes(req *http.Request, he *HTTPEvent, e *v2.Event) {
 	args := he.QueryArgs
 	if v, ok := args[reqID]; ok && v != "" {
 		e.SetID(v)
@@ -198,125 +196,4 @@ func mappingAttributesNew(req *http.Request, he *HTTPEvent, e *v2.Event) {
 	}
 	//e.SetExtension(extendAttributesRemoteIP, ctx.RemoteIP().String())
 	e.SetExtension(extendAttributesRemoteAddr, req.RemoteAddr)
-}
-
-func (c *httpSource) handleFastHTTP(ctx *fasthttp.RequestCtx) {
-
-	he := &HTTPEvent{
-		Path:      string(ctx.Path()),
-		Method:    string(ctx.Method()),
-		QueryArgs: getQueryArgs(ctx),
-		Headers:   getHeaders(ctx),
-	}
-
-	e := v2.NewEvent()
-	mappingAttributes(ctx, &e)
-
-	// try to convert request.Body to json
-	m := map[string]interface{}{}
-	err := json.Unmarshal(ctx.Request.Body(), &m)
-	if err == nil {
-		he.Body = m
-		e.SetExtension(extendAttributesBodyIsJSON, true)
-	} else {
-		he.Body = string(ctx.Request.Body())
-		e.SetExtension(extendAttributesBodyIsJSON, false)
-	}
-
-	if err = e.SetData(v2.ApplicationJSON, he.toMap()); err != nil {
-		ctx.Response.SetStatusCode(http.StatusBadRequest)
-		ctx.Response.SetBody([]byte(fmt.Sprintf("failed to set data: %s", err.Error())))
-		return
-	}
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	c.ch <- &cdkgo.Tuple{
-		Event: &e,
-		Success: func() {
-			ctx.Response.SetStatusCode(http.StatusOK)
-			wg.Done()
-		},
-		Failed: func(err2 error) {
-			ctx.Response.SetStatusCode(http.StatusInternalServerError)
-			ctx.Response.SetBody([]byte(
-				fmt.Sprintf("failed to send event to remote server: %s", err2.Error())))
-			wg.Done()
-		},
-	}
-	wg.Wait()
-}
-
-func mappingAttributes(ctx *fasthttp.RequestCtx, e *v2.Event) {
-	args := ctx.QueryArgs()
-	if args.Has(reqID) && len(args.Peek(reqID)) > 0 {
-		e.SetID(string(args.Peek(reqID)))
-	} else {
-		e.SetID(uuid.NewString())
-	}
-
-	if args.Has(reqSource) && len(args.Peek(reqSource)) > 0 {
-		e.SetSource(string(args.Peek(reqSource)))
-	} else {
-		e.SetSource(defaultSource)
-	}
-
-	if args.Has(reqType) && len(args.Peek(reqType)) > 0 {
-		e.SetType(string(args.Peek(reqType)))
-	} else {
-		e.SetType(defaultType)
-	}
-
-	if args.Has(reqSubject) && len(args.Peek(reqSubject)) > 0 {
-		e.SetSubject(string(args.Peek(reqSubject)))
-	}
-
-	if args.Has(reqSchema) && len(args.Peek(reqSchema)) > 0 {
-		e.SetDataSchema(string(args.Peek(reqSchema)))
-	}
-
-	e.SetExtension(extendAttributesRemoteIP, ctx.RemoteIP().String())
-	e.SetExtension(extendAttributesRemoteAddr, ctx.RemoteAddr().String())
-}
-
-func getQueryArgs(ctx *fasthttp.RequestCtx) map[string]string {
-	m := map[string]string{}
-	args := strings.Split(ctx.QueryArgs().String(), "&")
-	for _, arg := range args {
-		kv := strings.Split(arg, "=")
-		if len(kv) == 2 {
-			m[kv[0]] = kv[1]
-		}
-	}
-	return m
-}
-
-func getHeaders(ctx *fasthttp.RequestCtx) map[string]string {
-	m := map[string]string{}
-	r := bufio.NewReader(bytes.NewReader(ctx.Request.Header.Header()))
-	for {
-		l, isPrefix, err := r.ReadLine()
-		if err != nil {
-			break
-		}
-		var _l []byte
-		for isPrefix {
-			_l, isPrefix, err = r.ReadLine()
-			if err != nil {
-				break
-			}
-			l = append(l, _l...)
-		}
-		str := string(l)
-		idx := strings.Index(str, ":")
-		if idx == -1 {
-			// ignore something like POST /webhook?source=123&id=1234sda&type=xxxxx&subject=12eqsd&asdax=asdasd HTTP/1.1
-			continue
-		}
-		if idx+2 < len(str)-1 {
-			m[str[0:idx]] = str[idx+2:]
-		}
-	}
-
-	return m
 }
