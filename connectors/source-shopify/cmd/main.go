24207e4a574b9c69a3043053c143788fe828cd76
xdlbdy <xdlbdy@gmail.com>
2023-10-23 17:29:17 +0800

    feat: modify shopify source

--- connectors/source-shopify/cmd/main.go
+++ connectors/source-shopify/cmd/main.go
@@ -20,5 +20,5 @@ import (
 )
 
 func main() {
-	cdkgo.RunSource(internal.NewConfig, internal.NewShopifySource)
+	cdkgo.RunHttpSource(internal.NewConfig, internal.NewShopifySource)
 }
--- connectors/source-shopify/internal/server.go
+++ connectors/source-shopify/internal/server.go
@@ -16,111 +16,108 @@ package internal
 
 import (
 	"context"
-	"crypto/hmac"
-	"crypto/sha256"
-	"encoding/base64"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"hash"
-	"net"
+	"io"
 	"net/http"
-	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 
-	v2 "github.com/cloudevents/sdk-go/v2"
+	goshopify "github.com/bold-commerce/go-shopify/v3"
+	ce "github.com/cloudevents/sdk-go/v2"
 	"github.com/google/uuid"
-	"github.com/valyala/fasthttp"
+	"github.com/rs/zerolog"
 
 	cdkgo "github.com/vanus-labs/cdk-go"
 	"github.com/vanus-labs/cdk-go/log"
 )
 
 const (
-	name        = "Shopify Source"
-	defaultPort = 8080
+	name = "Shopify Source"
 
-	defaultSource              = "vanus-shopify-source"
-	extendAttributesOrderID    = "xvshopifyorderid"
-	extendAttributesTopic      = "xvshopifytopic"
-	extendAttributesWebhookID  = "xvshopifywebhookid"
-	extendAttributesShopDomain = "xvshopifydomain"
-	extendAttributesAPIVersion = "xvshopifyapiversion"
+	defaultSource               = "vanus-shopify-source"
+	extendAttributesTopic       = "xvshopifytopic"
+	extendAttributesWebhookID   = "xvshopifywebhookid"
+	extendAttributesShopDomain  = "xvshopifydomain"
+	extendAttributesAPIVersion  = "xvshopifyapiversion"
+	extendAttributesTriggeredAt = "xvshopifytriggeredat"
 
-	shopifyXHeaderAPIVersion = "X-Shopify-Api-Version"
-	shopifyXHeaderHmac       = "X-Shopify-Hmac-Sha256"
-	shopifyXHeaderOrderID    = "X-Shopify-Order-Id"
-	shopifyXHeaderShopDomain = "X-Shopify-Shop-Domain"
-	shopifyXHeaderTopic      = "X-Shopify-Topic"
-	shopifyXHeaderWebhookID  = "X-Shopify-Webhook-Id"
+	headerAPIVersion  = "X-Shopify-Api-Version"
+	headerHmac        = "X-Shopify-Hmac-Sha256"
+	headerShopDomain  = "X-Shopify-Shop-Domain"
+	headerTopic       = "X-Shopify-Topic"
+	headerWebhookID   = "X-Shopify-Webhook-Id"
+	headerTriggeredAt = "X-Shopify-Triggered-At"
 )
-
-var _ cdkgo.SourceConfigAccessor = &shopifySourceConfig{}
-
-type shopifySourceConfig struct {
-	cdkgo.SourceConfig `json:",inline" yaml:",inline"`
-	Port               int    `json:"port" yaml:"port"`
-	ClientSecret       string `json:"client_secret" yaml:"client_secret"`
-}
-
-func (c *shopifySourceConfig) GetSecret() cdkgo.SecretAccessor {
-	return nil
-}
-
-func NewConfig() cdkgo.SourceConfigAccessor {
-	return &shopifySourceConfig{}
-}
 
 var _ cdkgo.Source = &shopifySource{}
 
-func NewShopifySource() cdkgo.Source {
+func NewShopifySource() cdkgo.HTTPSource {
 	return &shopifySource{
 		ch: make(chan *cdkgo.Tuple, 1024),
 	}
 }
 
 type shopifySource struct {
-	cfg   *shopifySourceConfig
-	mutex sync.Mutex
-	ch    chan *cdkgo.Tuple
-	ln    net.Listener
-	hm    hash.Hash
+	ch     chan *cdkgo.Tuple
+	logger zerolog.Logger
+	app    goshopify.App
+	count  int64
+}
+
+func (c *shopifySource) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if !c.app.VerifyWebhookRequest(r) {
+		c.logger.Info().Msg("hmac invalid")
+		w.WriteHeader(http.StatusUnauthorized)
+		_, _ = w.Write([]byte("hmac invalid"))
+		return
+	}
+	atomic.AddInt64(&c.count, 1)
+	log.Info().Int64("total", atomic.LoadInt64(&c.count)).Msg("receive a new event")
+	topic := r.Header.Get(headerTopic)
+	e := ce.NewEvent()
+	e.SetID(uuid.NewString())
+	e.SetSource(defaultSource)
+	e.SetType(topic)
+	e.SetTime(time.Now())
+	body, _ := io.ReadAll(r.Body)
+	var m map[string]interface{}
+	json.Unmarshal(body, &m)
+	e.SetData(ce.ApplicationJSON, m)
+
+	e.SetExtension(extendAttributesWebhookID, r.Header.Get(headerWebhookID))
+	e.SetExtension(extendAttributesShopDomain, r.Header.Get(headerShopDomain))
+	e.SetExtension(extendAttributesAPIVersion, r.Header.Get(headerAPIVersion))
+	e.SetExtension(extendAttributesTriggeredAt, r.Header.Get(headerTriggeredAt))
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	c.ch <- &cdkgo.Tuple{
+		Event: &e,
+		Success: func() {
+			defer wg.Done()
+			c.logger.Info().Str("event_id", e.ID()).Msg("send event to target success")
+			w.WriteHeader(http.StatusOK)
+		},
+		Failed: func(err2 error) {
+			defer wg.Done()
+			c.logger.Warn().Interface("event_id", e.ID()).Err(err2).Msg("failed to send event to target")
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(
+				fmt.Sprintf("failed to send event to remote server: %s", err2.Error())))
+		},
+	}
+	wg.Wait()
 }
 
 func (c *shopifySource) Chan() <-chan *cdkgo.Tuple {
 	return c.ch
 }
 
-func (c *shopifySource) Initialize(_ context.Context, cfg cdkgo.ConfigAccessor) error {
-	_cfg, ok := cfg.(*shopifySourceConfig)
-	if !ok {
-		return errors.New("invalid config")
-	}
-
-	if _cfg.Port == 0 {
-		_cfg.Port = defaultPort
-	}
-	c.cfg = _cfg
-
-	c.hm = hmac.New(sha256.New, []byte(c.cfg.ClientSecret))
-
-	ln, err := net.Listen("tcp", fmt.Sprintf(":%d", c.cfg.Port))
-	if err != nil {
-		return err
-	}
-	c.ln = ln
-	go func() {
-		log.Info("Shopify source is ready to serving", map[string]interface{}{
-			"listen": c.cfg.Port,
-		})
-		if err := fasthttp.Serve(ln, c.handleFastHTTP); err != nil {
-			log.Error("failed to start http server", map[string]interface{}{
-				log.KeyError: err,
-			})
-			panic(err)
-		}
-	}()
+func (c *shopifySource) Initialize(ctx context.Context, config cdkgo.ConfigAccessor) error {
+	c.logger = log.FromContext(ctx)
+	cfg := config.(*shopifySourceConfig)
+	c.app = goshopify.App{ApiSecret: cfg.ClientSecret}
 	return nil
 }
 
@@ -129,68 +126,5 @@ func (c *shopifySource) Name() string {
 }
 
 func (c *shopifySource) Destroy() error {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	if err := c.ln.Close(); err != nil {
-		log.Warning("close listener error", map[string]interface{}{
-			log.KeyError: err,
-		})
-	}
 	return nil
-}
-
-func (c *shopifySource) handleFastHTTP(ctx *fasthttp.RequestCtx) {
-	body := ctx.PostBody()
-	h := hmac.New(sha256.New, []byte(c.cfg.ClientSecret))
-	h.Write(body)
-	hmacCalculated := base64.StdEncoding.EncodeToString(h.Sum(nil))
-	hmacHeader := string(ctx.Request.Header.Peek(shopifyXHeaderHmac))
-
-	// validate signature
-	if !strings.EqualFold(hmacCalculated, hmacHeader) {
-		ctx.Response.SetStatusCode(http.StatusUnauthorized)
-		return
-	}
-
-	topic := string(ctx.Request.Header.Peek(shopifyXHeaderTopic))
-	e := v2.NewEvent()
-	e.SetID(uuid.NewString())
-	e.SetSource(defaultSource)
-	e.SetType(topic)
-	e.SetTime(time.Now())
-	m := map[string]interface{}{}
-	err := json.Unmarshal(ctx.Request.Body(), &m)
-
-	if err = e.SetData(v2.ApplicationJSON, m); err != nil {
-		ctx.Response.SetStatusCode(http.StatusBadRequest)
-		ctx.Response.SetBody([]byte(fmt.Sprintf("failed to set data: %s", err.Error())))
-		return
-	}
-
-	e.SetExtension(extendAttributesOrderID, string(ctx.Request.Header.Peek(shopifyXHeaderOrderID)))
-	e.SetExtension(extendAttributesTopic, topic)
-	e.SetExtension(extendAttributesWebhookID, string(ctx.Request.Header.Peek(shopifyXHeaderWebhookID)))
-	e.SetExtension(extendAttributesShopDomain, string(ctx.Request.Header.Peek(shopifyXHeaderShopDomain)))
-	e.SetExtension(extendAttributesAPIVersion, string(ctx.Request.Header.Peek(shopifyXHeaderAPIVersion)))
-
-	log.Debug("received a HTTP Request, ready to send", map[string]interface{}{
-		"event": e.String(),
-	})
-
-	wg := sync.WaitGroup{}
-	wg.Add(1)
-	c.ch <- &cdkgo.Tuple{
-		Event: &e,
-		Success: func() {
-			ctx.Response.SetStatusCode(http.StatusOK)
-			wg.Done()
-		},
-		Failed: func(err2 error) {
-			ctx.Response.SetStatusCode(http.StatusInternalServerError)
-			ctx.Response.SetBody([]byte(
-				fmt.Sprintf("failed to send event to remote server: %s", err2.Error())))
-			wg.Done()
-		},
-	}
-	wg.Wait()
 }
